cc05e5bb534521fb2f8e5240cb104a03adbf3679
kawano <kawano.fanta@gmail.com>
2014-08-09 23:46:43 +0900

    device設定テスト確認済、correct_user未完

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :correct_user
+  before_filter :authenticate_user!
+  #before_filter :correct_user
 
   private
   def correct_user
