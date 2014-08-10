18cb6d76d1a0996f0de4b46ecad623a4ac65c633
kawano <kawano.fanta@gmail.com>
2014-08-10 11:54:21 +0900

    通常サインアップ時のuidフィールドの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :omniauthable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end
