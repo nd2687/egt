52670dafd8da54c564f604b2a3c00c4daebd5771
kawano <kawano.fanta@gmail.com>
2014-08-15 02:52:40 +0900

    デザイン修正

--- app/assets/javascripts/application/word.js
+++ app/assets/javascripts/application/word.js
@@ -2,14 +2,16 @@ $(function (){
   $("li.word").hover(function (){
     var japanese = $(this).children('.japanese');
     var english = $(this).children('.english');
-    japanese.stop().hide("");
-    english.stop().show("");
+    $(this).css('cursor', 'pointer').css('background', '#a3d57c');
+    japanese.stop().slideUp();
+    english.stop().slideDown();
   },
     function (){
       var japanese = $(this).children('.japanese');
       var english = $(this).children('.english');
-      japanese.stop().show("");
-      english.stop().hide("");
+      $(this).css('cursor', 'pointer').css('background', '');
+      japanese.stop().slideDown();
+      english.stop().slideUp();
     }
   );
 })
