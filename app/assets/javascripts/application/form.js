03db30456b9ac7f27bf485cb338ebc1948249320
kawano <kawano.fanta@gmail.com>
2014-08-17 02:20:41 +0900

    ナビ スクロール表示やめる

--- app/assets/javascripts/application/form.js
+++ app/assets/javascripts/application/form.js
@@ -15,24 +15,24 @@ $(function() {
   var showFlag = false;
   nav.css('top', -navHeight+'px');
   //ナビゲーションの位置まできたら表示
-  $(window).scroll(function () {
-    var winTop = $(this).scrollTop();
-    if (winTop >= navTop) {
-      if (showFlag == false) {
-        showFlag = true;
-        nav
-    .addClass('fixed')
-    .stop().animate({'top' : '0px'}, 200);
-      }
-    } else if (winTop <= navTop) {
-      if (showFlag) {
-        showFlag = false;
-        nav.stop().animate({'top' : -navHeight+'px'}, 200, function(){
-          nav.removeClass('fixed');
-        });
-      }
-    }
-  });
+  //$(window).scroll(function () {
+  //  var winTop = $(this).scrollTop();
+  //  if (winTop >= navTop) {
+  //    if (showFlag == false) {
+  //      showFlag = true;
+  //      nav
+  //  .addClass('fixed')
+  //  .stop().animate({'top' : '0px'}, 200);
+  //    }
+  //  } else if (winTop <= navTop) {
+  //    if (showFlag) {
+  //      showFlag = false;
+  //      nav.stop().animate({'top' : -navHeight+'px'}, 200, function(){
+  //        nav.removeClass('fixed');
+  //      });
+  //    }
+  //  }
+  //});
 
   $('div.nav').hover( function (){
     if (showFlag == false) {
