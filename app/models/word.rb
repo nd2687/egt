d0d2762e34ec19dbbba03f482fc6f05fa865af50
kawano <kawano.fanta@gmail.com>
2014-08-17 21:20:37 +0900

    validationエラーメッセージ途中

--- app/models/word.rb
+++ app/models/word.rb
@@ -13,8 +13,8 @@
 class Word < ActiveRecord::Base
   belongs_to :user
 
-  validates :english, presence: true, length: { maximum: 21 },
-    format: { with: /\A[0-9A-Za-z\s]+\z/, on: :create }
-  validates :japanese, presence: true, length: { maximum: 12 },
-    format: { with: /\A[^\x01-\x7E]+\z/, on: :create }
+  validates :english, presence: { message: 'presence NG' }, length: { maximum: 21, message: 'length NG' },
+    format: { with: /\A[0-9A-Za-z\s]+\z/, on: :create, message: 'format NG' }
+  validates :japanese, presence: { message: 'presence NG' }, length: { maximum: 12, message: 'length NG' },
+    format: { with: /\A[^\x01-\x7E]+\z/, on: :create, message: 'format NG' }
 end
