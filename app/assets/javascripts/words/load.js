fcbc70eab82acac64ef6970049bbec1b480258c5
kawano <kawano.fanta@gmail.com>
2014-08-25 16:53:43 +0900

    完了ソート

--- app/assets/javascripts/words/load.js
+++ app/assets/javascripts/words/load.js
@@ -2,7 +2,8 @@ $(function (){
   $("button.sortButton").on('click', function(){
     var sortType = $(this).attr('data-sort-type');
     var wordType = $(this).attr('data-word-type');
+    var completeType = $(this).attr('data-complete-type');
     $("ul").fadeOut('fast');
-    window.location = "/?sort_type="+sortType+"&word_type="+wordType;
+    window.location = "/?sort_type="+sortType+"&word_type="+wordType+"&complete_type="+completeType;
   });
 })
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,13 @@ class HomeController < ApplicationController
   def index
     sort_type = params[:sort_type].present? || params[:sort_type] == "japanese" ?
       params[:sort_type] : "updated_at desc"
-    @words = current_user.words.all.where(complete: false).order("#{sort_type}").
-      paginate(:per_page => 100, :page => params[:page])
+    if params[:complete_type]
+      @words = current_user.words.all.where(complete: true).order("#{sort_type}").
+        paginate(:per_page => 100, :page => params[:page])
+    else
+      @words = current_user.words.all.where(complete: false).order("#{sort_type}").
+        paginate(:per_page => 100, :page => params[:page])
+    end
     if request.xhr?
       sleep(1)
       render :partial => "words/more_words"
