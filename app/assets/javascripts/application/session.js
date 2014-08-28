77302ef83c2b57641fa29fd884f5ca3f3583d86b
kawano <kawano.fanta@gmail.com>
2014-08-29 03:43:20 +0900

    アラート修正

--- app/assets/javascripts/application/session.js
+++ app/assets/javascripts/application/session.js
@@ -1,3 +1,20 @@
 $(function(){
-  $('p.alert').slideDown('slow').delay(2000).slideUp('slow');
+  $( ".alert" ).dialog({
+    autoOpen: false,
+    show: "clip",
+    hide: "fade",
+    height: 50,
+    closeOnEscape: true,
+    draggable: false,
+    position: {
+      my: "center",
+      at: "center",
+      of: window
+    },
+    open: function(event, ui){
+      setTimeout("$('.alert').dialog('close')",2500);
+    }
+  });
+  $(".ui-dialog-titlebar").hide();
+  $( ".alert" ).parent().css({position:"fixed"}).end().dialog( "open" );
 })
